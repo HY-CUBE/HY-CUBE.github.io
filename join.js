58c388e6a730d4c7680f1a408e0a7d0d0c8bdfc7
HY-CUBE <hycube17@gmail.com>
2017-03-17 20:02:48 +0900

    Add files via upload

---
+++ join.js
@@ -0,0 +1,16 @@
+
+function joinUs(){
+    var form = $('.student'),
+        student = form.form('get values');
+    $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbzo2qnAbYUir8B3vuzjbVQW5AVWY4OeM1w2DkDqotwYiwm2FZ8k/exec",
+        data: {
+            Name:student.Name,
+            Student_ID:student.ID,
+            Email:student.Email,
+            Phone_Num:student.Phone_Num,
+            Grade:student.Grade
+        },
+        type: "POST",
+    });
+}
